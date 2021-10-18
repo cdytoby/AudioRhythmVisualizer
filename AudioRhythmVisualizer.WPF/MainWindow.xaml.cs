4ddf09ab78832ec89fc7d155e19229095787e758
cdytoby <cdytoby@live.cn>
2021-10-18 22:20:26 +0200

    Some small improvements

--- AudioRhythmVisualizer.WPF/MainWindow.xaml.cs
+++ AudioRhythmVisualizer.WPF/MainWindow.xaml.cs
@@ -7,11 +7,8 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
 using AudioRhythmVisualizer.Core.ViewModel;
-using NAudio.CoreAudioApi;
-using NAudio.Mixer;
 using NAudio.Utils;
 using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
 using ScottPlot.Control;
 using ScottPlot.Plottable;
 using Color = System.Drawing.Color;
@@ -28,19 +25,18 @@ namespace AudioRhythmVisualizer.WPF
 		private DispatcherTimer dispatcherTimerPlayback;
 		private DispatcherTimer dispatcherTimerGraphUpdate;
 		private DispatcherTimer dispatcherTimerSFX;
-		private SignalPlot audioSignalPlot;
+		private SignalPlotConst<double> audioSignalPlot;
 		private VLine playbackLine;
 		private VLine cursorMarkLine;
 		private VLine[] beatsLines;
-		private Text[] beatsLineLabels;
+		private Text[] beatsTexts;
 
 		private WaveOut mainWaveOut;
+		private SoundPlayer soundPlayer;
 
 		private TimeSpan startPosition;
 		private double currentTimePosition;
 		private int nextBeatIndex;
-
-		private SoundPlayer soundPlayer;
 
 		public MainWindow()
 		{
@@ -62,9 +58,13 @@ namespace AudioRhythmVisualizer.WPF
 
 		private void SetupPlot()
 		{
+			mainPlot.Configuration.DoubleClickBenchmark = true;
 			mainPlot.Configuration.Quality = QualityMode.Low;
 			mainPlot.Configuration.UseRenderQueue = true;
 			mainPlot.Configuration.LockVerticalAxis = true;
+			mainPlot.Configuration.MiddleClickDragZoom = false;
+			mainPlot.Configuration.RightClickDragZoom = false;
+			mainPlot.Plot.Legend(false);
 
 			mainPlot.RightClicked -= mainPlot.DefaultRightClickEvent;
 			mainPlot.RightClicked += OnRightClicked;
@@ -103,7 +103,10 @@ namespace AudioRhythmVisualizer.WPF
 			cursorMarkLine = null;
 
 			audioSignalPlot =
-				mainPlot.Plot.AddSignal(viewModel.audioDataChannel0, viewModel.audioFormat.SampleRate, Color.DodgerBlue);
+				mainPlot.Plot.AddSignalConst(viewModel.audioDataChannel0, viewModel.audioFormat.SampleRate, Color.DodgerBlue);
+			mainPlot.Plot.XAxis.Grid(false);
+			mainPlot.Plot.YAxis.Grid(false);
+			mainPlot.Plot.XAxis.TickDensity(0.5d);
 			mainPlot.Plot.Title($"SampleRate: {viewModel.audioFormat.SampleRate}, {viewModel.audioFormat.Channels} Channel");
 			mainPlot.Plot.SetOuterViewLimits(0, viewModel.fileStream.TotalTime.TotalSeconds + 5, -1, 1);
 			mainPlot.Plot.XLabel($"Time (seconds), total length={viewModel.audioTimeLength}s");
@@ -136,13 +139,25 @@ namespace AudioRhythmVisualizer.WPF
 		private void BeatDataReady_RefreshPlot()
 		{
 			beatsLines = new VLine[viewModel.beatsData.Length];
-			beatsLineLabels = new Text[viewModel.beatsData.Length];
+			beatsTexts = new Text[viewModel.beatsData.Length];
 			for (int i = 0; i < viewModel.beatsData.Length; i++)
 			{
 				beatsLines[i] = mainPlot.Plot.AddVerticalLine(viewModel.beatsData[i], Color.SeaGreen);
-				beatsLineLabels[i] =
-					mainPlot.Plot.AddText(i.ToString(), viewModel.beatsData[i], -0.5f, 12f, Color.Chocolate);
-				beatsLineLabels[i].IsVisible = false;
+				beatsTexts[i] = mainPlot.Plot.AddText(i.ToString(), viewModel.beatsData[i], -0.5f, 10f, Color.Coral);
+				beatsTexts[i].IsVisible = labelVisibleCheckbox.IsChecked.GetValueOrDefault();
+			}
+		}
+
+		private void SetShowBeatIndex(bool shouldShow)
+		{
+			if (beatsTexts == null)
+			{
+				return;
+			}
+
+			foreach (Text t in beatsTexts)
+			{
+				t.IsVisible = shouldShow;
 			}
 		}
 
@@ -159,7 +174,7 @@ namespace AudioRhythmVisualizer.WPF
 					mainPlot.Plot.Remove(vLine);
 				}
 			}
-			foreach (Text text in beatsLineLabels)
+			foreach (Text text in beatsTexts)
 			{
 				if (text != null)
 				{
@@ -167,7 +182,7 @@ namespace AudioRhythmVisualizer.WPF
 				}
 			}
 			beatsLines = null;
-			beatsLineLabels = null;
+			beatsTexts = null;
 		}
 
 		private void SetPlaybackPosition(double timePositionSecond)
@@ -215,6 +230,7 @@ namespace AudioRhythmVisualizer.WPF
 					if (cursorMarkLine != null)
 					{
 						SetPlaybackPosition(cursorMarkLine.X);
+						ClearCursorMark(this, null);
 					}
 					mainWaveOut.Play();
 					playbackLine.IsVisible = true;
@@ -233,7 +249,7 @@ namespace AudioRhythmVisualizer.WPF
 			double mouseCoordinate = mainPlot.GetMouseCoordinates().x;
 			if (cursorMarkLine == null)
 			{
-				cursorMarkLine = mainPlot.Plot.AddVerticalLine(mouseCoordinate);
+				cursorMarkLine = mainPlot.Plot.AddVerticalLine(mouseCoordinate, Color.DarkRed);
 			}
 			else
 			{
@@ -273,6 +289,20 @@ namespace AudioRhythmVisualizer.WPF
 				soundPlayer.Play();
 				nextBeatIndex++;
 			}
+		}
+
+		private void ClearCursorMark(object sender, RoutedEventArgs e)
+		{
+			if (cursorMarkLine != null)
+			{
+				mainPlot.Plot.Remove(cursorMarkLine);
+				cursorMarkLine = null;
+			}
+		}
+
+		private void ShowTextChanged(object sender, RoutedEventArgs e)
+		{
+			SetShowBeatIndex(labelVisibleCheckbox.IsChecked.GetValueOrDefault());
 		}
 	}
 }
